5fba7026983193e6e218cfe9c1bfd7d15ba4716d
Muhammad Bilal Khan <80154914+BILAL-KHAN-0304@users.noreply.github.com>
2021-03-19 00:30:02 +0500

    Version 0.0.1

---
+++ Ori And the Blind Forest/Assets/ori.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ori : MonoBehaviour
+{
+    public int speed = 3;
+    public Rigidbody2D rigidbody;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        Debug.Log("move");
+        rigidbody.velocity = new Vector3(move*speed, rigidbody.velocity.y, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.y, speed);
+        }
+    }
+}
